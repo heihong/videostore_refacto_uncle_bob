ccb92e6566447c94dbe8f0d214368314a985d9de
mavomo <michelle.avomo@gmail.com>
2018-01-29 22:40:08 +0100

    init Java version

---
+++ java/src/main/java/fr/soat/cleancoders/Rental.java
@@ -0,0 +1,19 @@
+package fr.soat.cleancoders;
+
+public class Rental {
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    private Movie movie;
+    private int daysRented;
+}
