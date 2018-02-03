087443b8545f6b5648fb6fd2f4af8e7619f812df
Romain Deneau <romain.deneau@soat.fr>
2018-02-03 23:02:31 +0100

    Refactor `Report` to small functions

--- typescript/src/report.ts
+++ typescript/src/report.ts
@@ -1,31 +1,42 @@
-import { Movie }  from './movie';
 import { Rental } from './rental';
 
 export class Report {
     private _rentals: Rental[] = [];
 
-    constructor(readonly customerName: string) {
-    }
+    constructor(readonly customerName: string) { }
 
     addRental(rental: Rental) {
         this._rentals.push(rental);
     }
 
     print() {
-        let frequentRenterPoints = 0;
-        let totalAmount = 0;
-        let result = 'Rental Record for ' + this.customerName + '\n';
+        return this.printHeader() +
+               this.printLines() +
+               this.printFooter(
+                    this.computeTotalAmount(),
+                    this.computeFrequentRenterPoints());
+    }
 
-        for (let rental of this._rentals) {
-            let currentAmount = rental.computeAmount();
-            frequentRenterPoints += rental.computePoints();
-            result += '\t' + rental.movie.title + '\t' + currentAmount.toFixed(1) + '\n';
-            totalAmount += currentAmount;
-        }
+    private printFooter(totalAmount: number, frequentRenterPoints: number) {
+        return 'You owed ' + totalAmount.toFixed(1) + '\n'
+            + 'You earned ' + frequentRenterPoints + ' frequent renter points \n';
+    }
 
-        result += 'You owed ' + totalAmount.toFixed(1) + '\n';
-        result += 'You earned ' + frequentRenterPoints + ' frequent renter points \n';
+    private printHeader() {
+        return 'Rental Record for ' + this.customerName + '\n';
+    }
 
-        return result;
+    private printLines() {
+        return this._rentals.reduce((result, rental) => result
+            + '\t' + rental.movie.title
+            + '\t' + rental.computeAmount().toFixed(1) + '\n', '');
+    }
+
+    private computeFrequentRenterPoints() {
+        return this._rentals.reduce((result, rental) => result + rental.computePoints(), 0);
+    }
+
+    private computeTotalAmount() {
+        return this._rentals.reduce((result, rental) => result + rental.computeAmount(), 0);
     }
 }
