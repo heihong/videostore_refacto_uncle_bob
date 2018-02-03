8dbbf6238797babc2562919b233b8eff3c9760b3
Romain Deneau <romain.deneau@soat.fr>
2018-02-03 22:44:38 +0100

    Move `computeXxx()` from `Report` to `Rental` → Fix code smell : "Feature envy"

--- typescript/src/rental.ts
+++ typescript/src/rental.ts
@@ -1,8 +1,36 @@
 import { Movie } from "./movie";
 
 export class Rental {
-    public constructor(
+    constructor(
         readonly movie: Movie,
         readonly daysRented: number,
     ) {}
+
+    computeAmount() {
+        let amount = 0;
+        switch (this.movie.priceCode) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (this.daysRented > 2) {
+                    amount += (this.daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                amount += this.daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                amount += 1.5;
+                if (this.daysRented > 3) {
+                    amount += (this.daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return amount;
+    }
+
+    computePoints() {
+        return this.movie.priceCode == Movie.NEW_RELEASE && this.daysRented > 1
+            ? 2
+            : 1;
+    }
 }
