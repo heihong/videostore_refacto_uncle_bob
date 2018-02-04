0926424cef885f56ec6c143f7d4fc13079422c79
Romain Deneau <romain.deneau@soat.fr>
2018-02-04 13:56:57 +0100

    Add `Rental.computeAmount()` specs

--- typescript/src/rental.spec.ts
+++ typescript/src/rental.spec.ts
@@ -2,6 +2,33 @@ import { Movie  } from './movie';
 import { Rental } from './rental';
 
 describe('VideoStore: Rental', () => {
+    describe('computeAmount()', () => {
+        given()
+            .aNewReleaseMovie()
+            .withDaysRentedIn(0, 1, 2, 3, 4, 8)
+            .expectRentalAmountToBe(0, 3, 6, 9, 12, 24);
+
+        given()
+            .aChildrenMovie()
+            .withDaysRentedIn(0, 1, 2, 3)
+            .expectRentalAmountToBe(1.5);
+
+        given()
+            .aChildrenMovie()
+            .withDaysRentedIn(4, 5, 6)
+            .expectRentalAmountToBe(3, 4.5, 6);
+
+        given()
+            .aRegularMovie()
+            .withDaysRentedIn(0, 1, 2)
+            .expectRentalAmountToBe(2);
+
+        given()
+            .aRegularMovie()
+            .withDaysRentedIn(3, 4, 8)
+            .expectRentalAmountToBe(3.5, 5, 11);
+    });
+
     describe('computePoints()', () => {
         given()
             .aRegularMovie()
@@ -57,12 +84,21 @@ describe('VideoStore: Rental', () => {
                 super(priceCode, label);
             }
 
-            expectRentalPointsToBe(expected: number) {
+            expectRentalAmountToBe(...expected: number[]) {
+                this.expect(sut => sut.computeAmount(), expected);
+            }
+
+            expectRentalPointsToBe(...expected: number[]) {
+                this.expect(sut => sut.computePoints(), expected);
+            }
+
+            private expect(computeResult: (sut: Rental) => number, allExpected: number[]) {
                 describe(`given a ${this.label} movie`, () => {
-                    this.allDaysRented.forEach(daysRented => {
+                    this.allDaysRented.forEach((daysRented, index) => {
+                        const expected = allExpected[Math.min(index, allExpected.length - 1)];
                         it(`should return ${expected} given ${daysRented} day(s) rented`, () => {
                             const sut = new Rental(new Movie('', this.priceCode), daysRented);
-                            const result = sut.computePoints();
+                            const result = computeResult(sut);
                             expect(result).toBe(expected);
                         });
                     });
